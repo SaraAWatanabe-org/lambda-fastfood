38ae043a42260def9ccad685991b08c6ec7f1dce
Marcelo C <marcelo.c.b22@gmail.com>
2024-09-28 17:35:13 -0300

    Terraform para criação do recurso na AWS.

---
+++ terraform/lambda.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_cognito_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "cognito_login_function" {
+  function_name = var.function_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = var.handler
+  runtime       = "java11"
+
+  filename = "base_lambda.jar"
+
+
+  environment {
+    variables = {
+      CLIENT_ID = var.client_id
+    }
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.lambda_policy_attach]
+}
